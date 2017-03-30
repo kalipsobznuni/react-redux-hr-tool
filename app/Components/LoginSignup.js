874bd35a2a5222ccec5fea065622db0d72e7d5d9
Erik Davtyan <erikdavtyan@gmail.com>
2017-03-30 10:15:26 +0400

    uncontrolled inputs

--- app/Components/LoginSignup.js
+++ app/Components/LoginSignup.js
@@ -1,15 +1,8 @@
 import React from 'react';
 
 export default class LoginSignup extends React.PureComponent {
-  constructor() {
-    super();
-    this.state = {
-      email: "",
-      password: ""
-    }
-  }
   logIn = () => {
-    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch(error => {
+    firebase.auth().signInWithEmailAndPassword(this.email.value, this.password.value).catch(error => {
       console.log(error);
     });
   }
@@ -18,8 +11,8 @@ export default class LoginSignup extends React.PureComponent {
       <div className="main">
         <div className="container">
           <div className="login-signup">
-            <input value={this.state.email} onChange={e => this.setState({email: e.currentTarget.value})} placeholder="Email"></input>
-            <input value={this.state.password} onChange={e => this.setState({password: e.currentTarget.value})} type="password" placeholder="Password"></input>
+            <input ref={email => this.email = email} placeholder="Email"></input>
+            <input ref={password => this.password = password} type="password" placeholder="Password"></input>
             <button onClick={this.logIn}>Log In</button>
             <button> Sign Up </button>
           </div>
