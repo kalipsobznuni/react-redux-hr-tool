55257bde00b15a00254220c11a4d4106cb96b41d
kalipsobznuni <kalipso.bznuni14159@gmail.com>
2017-03-30 16:45:03 +0400

    random candidates list

---
+++ app/candidates.js
@@ -0,0 +1,8 @@
+const Candidates =[
+  {name:"David Hakobyan", profession: "Developer", status:"Shortlisted"},
+  {name:"Ann Brown", profession: "Developer", status:"Rejected"},
+  {name:"Maria Sharapova", profession: "Designerr", status:"Accepted"},
+  {name:"Erik Davtyan", profession: "Developer", status:"Denied"},
+  {name:"Hakob Paronyan", profession: "Engineer", status:"Shortlisted"},
+  {name:"Hillary Banks", profession: "Designer", status:"Accepted"},
+]
